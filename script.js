dcd0e16c8d560da7060db62eabb107214125c377
Shivam SF <shukla.shivam1211@gmail.com>
2024-04-02 14:32:35 +0530

    for captcha changes

--- script.js
+++ script.js
@@ -1,14 +1,28 @@
-
-function beforeSubmit(){
+let captchaChecked = false;
+function beforeSubmit(event){
+    if(captchaChecked){
     let setLeadDate = document.querySelector(".setLeadDate");
     let inputLeadDate = document.querySelector(".inputLeadDate");
     console.log(inputLeadDate.value);
 
     let formattedDate = new Date(inputLeadDate.value).toLocaleDateString("en-US");
     setLeadDate.value = formattedDate;
+    }
+    else{
+        alert("Please check the recpatcha box to submit the form");
+        
+        //this method will stop the submission of the form
+        event.preventDefault();
+    }
 }
 
 
 function timestamp() { 
     var response = document.getElementById("g-recaptcha-response"); if (response == null || response.value.trim() == "") {var elems = JSON.parse(document.getElementsByName("captcha_settings")[0].value);elems["ts"] = JSON.stringify(new Date().getTime());document.getElementsByName("captcha_settings")[0].value = JSON.stringify(elems); } 
-} setInterval(timestamp, 500); 
+} 
+
+setInterval(timestamp, 500); 
+
+function captchasuccess{
+    captchaChecked = true;
+}
